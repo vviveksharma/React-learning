67240159e74d4beeeb1e91a12f6a7dd69d234daa
vviveksharma <visharma@progress.com>
2023-08-20 03:33:42 +0530

    Learning from the TODO
    Signed-off-by: vviveksharma <visharma@progress.com>

--- todo/src/App.js
+++ todo/src/App.js
@@ -1,23 +1,64 @@
-import logo from './logo.svg';
-import './App.css';
+import { useState } from "react";
+import "./App.css";
+import { Task } from "./Task";
 
 function App() {
+  const [task, setTask] = useState([]);
+  const [newTask, setNewTask] = useState("");
+
+  const HandleChange = (e) => {
+    setNewTask(e.target.value);
+  };
+
+  const addTask = () => {
+    const todo = {
+      id: task.length === 0 ? 1 : task[task.length - 1].id + 1,
+      taskName: newTask,
+      complete: false,
+    };
+    setTask([...task, todo]);
+  };
+
+  const delTask = (e) => {
+    setTask(
+      task.filter((taskName) => {
+        return e !== taskName.id;
+      })
+    );
+  };
+
+  const compTask = (e) => {
+    setTask(
+      task.map((todo) => {
+        if (todo.id === e) {
+          console.log("Got inside the if")
+          return { ...todo, complete: true };
+        } else {
+          return todo;
+        }
+      })
+    );
+  };
+
   return (
     <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+      <div className="addTask">
+        <input onChange={HandleChange} />
+        <button onClick={addTask}>Submit</button>
+      </div>
+      <div className="list">
+        {task.map((t) => {
+          return (
+            <Task
+              deleteTask={delTask}
+              taskName={t.taskName}
+              id={t.id}
+              completed={t.complete}
+              completeTask={compTask}
+            />
+          );
+        })}
+      </div>
     </div>
   );
 }
